cbda42b2646260deb0330270eae75d556c1fe050
Muhammad Masood <muhammadmasood673@gmail.com>
2023-01-14 17:38:40 +0500

    create schema in apollo/server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone'
+
+
+
+const typeDefs = `#graphql
+
+type Book {
+    title:String
+    author:String
+}
+
+type Query {
+    books:[Book]
+}
+
+`
